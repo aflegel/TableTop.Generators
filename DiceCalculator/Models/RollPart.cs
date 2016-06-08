a5f60abbbfee3d90875e90321e4a884fb8ae2d3a
Alex <alexander.flegel@omnilogic.net>
2016-06-08 10:14:44 -0600

    added some new objects to deal with database models.

---
+++ DiceCalculator/Models/RollPart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceCalculator.Models
+{
+	class RollPart
+	{
+		byte faceId;
+
+		int rollMasterId;
+	}
+}
